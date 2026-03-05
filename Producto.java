31612abf1ee7f60e934bd1ade9b698d47a70eaf5
alejandro gutierrez <edisongutierrez1146905@correo.itm.edu.co>
2026-03-04 20:52:47 -0500

    ajuste del punto 6 creacion del punto2

--- Producto.java
+++ Producto.java
@@ -2,10 +2,35 @@ public class Producto {
     String Nombre;
     double Precio;
     boolean Disponibilidad;
+
     public Producto(String nombre, double precio, boolean disponibilidad) {
         Nombre = nombre;
         Precio = precio;
         Disponibilidad = disponibilidad;
     }
-    
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double precio) {
+        Precio = precio;
+    }
+
+    public boolean isDisponibilidad() {
+        return Disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        Disponibilidad = disponibilidad;
+    }
+
 }
--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.net.Socket;
 import java.util.Scanner;
 
 public class main {
@@ -5,80 +6,95 @@ public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Metodos m = new Metodos();
+        System.out.println("Ingrese tamaño de la matriz: ");
+        int n = m.validarNumero(sc);
+        boolean seguir = true;
+        while (seguir) {
+            int opt = 0;
 
-        int opt = 0;
+            System.out.println("Bienvenido al Menu:");
+            System.out.println("----------------------");
+            System.out.println("INDIQUE A QUE PUNTO QUIERE INGRESAR");
+            System.out.println("1.punto #1");
+            System.out.println("2.punto #2");
+            System.out.println("3.punto #3");
+            System.out.println("4.punto #4");
+            System.out.println("5.punto #5");
+            System.out.println("6.punto #6");
+            System.out.println("7.punto #7");
+            System.out.println("8.punto #8");
+            System.out.println("9.punto #9");
+            System.out.println("10.punto #10");
+            System.out.println("--------------------");
+            System.out.println("indique una opcion: ");
+            opt = m.validarNumero(sc);
 
-        System.out.println("Bienvenido al Menu:");
-        System.out.println("----------------------");
-        System.out.println("INDIQUE A QUE PUNTO QUIERE INGRESAR");
-        System.out.println("1.punto #1");
-        System.out.println("2.punto #2");
-        System.out.println("3.punto #3");
-        System.out.println("4.punto #4");
-        System.out.println("5.punto #5");
-        System.out.println("6.punto #6");
-        System.out.println("7.punto #7");
-        System.out.println("8.punto #8");
-        System.out.println("9.punto #9");
-        System.out.println("10.punto #10");
-        System.out.println("--------------------");
-        System.out.println("indique una opcion: ");
-        opt = sc.nextInt();
+            switch (opt) {
+                case 1:
+                    // System.out.println("hola");
+                    m.LlamarMetodos();
+                    break;
+                case 2:
 
-        switch (opt) {
-            case 1:
-                // System.out.println("hola");
-                m.LlamarMetodos();
-                break;
-            case 2:
+                    Productop2[][] matriz = m.iniciarMatrizp2(n);
+                    m.llenarMatriz(matriz);
+                    int total = m.sumarCantidad(matriz);
 
-                break;
-            case 3:
+                    System.out.println("El total de inventario es: " + total);
 
-                break;
-            case 4:
-            
+                    break;
+                case 3:
 
-                System.out.println("Ingrese tamaño de la matriz: ");
-                int n = sc.nextInt();
+                    break;
+                case 4:
 
-                Puestos[][] matriz = m.iniciarMatriz(n);
+                    Puestos[][] m4 = m.iniciarMatriz(n);
 
-                m.llenarMatriz(matriz);
-                m.ordenarFilasPorPrecio(matriz);
+                    m.llenarMatriz(m4);
+                    m.ordenarFilasPorPrecio(m4);
 
-                System.out.println("Puestos en orden por precio en cada fila: ");
+                    System.out.println("Puestos en orden por precio en cada fila: ");
 
-                for (int i = 0; i < matriz.length; i++) {
-                    for (int j = 0; j < matriz[i].length; j++) {
+                    for (int i = 0; i < m4.length; i++) {
+                        for (int j = 0; j < m4[i].length; j++) {
 
-                        System.out.println(
-                                "Asiento " + matriz[i][j].getNumero() + " - Precio " + matriz[i][j].getPrecio());
+                            System.out.println(
+                                    "Asiento " + m4[i][j].getNumero() + " - Precio " + m4[i][j].getPrecio());
+
+                        }
 
                     }
 
-                }
+                    break;
+                case 5:
 
-                break;
-            case 5:
+                    break;
+                case 6:
 
-                break;
-            case 6:
+                    break;
+                case 7:
 
-                break;
-            case 7:
+                    break;
+                case 8:
 
-                break;
-            case 8:
+                    break;
+                case 9:
 
-                break;
-            case 9:
+                    break;
+                case 10:
 
-                break;
+                    break;
+                case 11:
+                    System.out.println("Suerte es que le digo");
+                    seguir = false;
+                    break;
 
-            default:
-                break;
+                default:
+                    System.out.println("hasta luego mi cuate");
+                    break;
+            }
+
         }
-
     }
+
 }
